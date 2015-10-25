c48a46f61f686abdf0a706120433f511fb2abb1d
robscott3 <roberts2@spc.int>
2015-10-26 10:29:37 +1100

    mfcl defined

--- R/mfcl.r
+++ R/mfcl.r
@@ -1,26 +1,11 @@
-#' mfcl
-#'
-#' Runs MFCL for specified .frq and .par files.
-#' The version of MFCL to be run can be set using the function 'setMFCLversion'
-#'
-#' @param frq:  An object of class MFCLFrq
-#' @param par:  An object of class MFCLPar
-#' @param outpar: Character string specifying the name of the output par file.
-#' @param switch: Numeric vector of additional flag settings (optional)
-#'
-#' @return Returns nothing: MFCL output files are written to the working directory
-#'
-#' @examples
-#' mfcl(skj.frq, skj.par "out.par", switch=c(1, 1,1,1))
-#'
-#' @export
+
 
 
 #' @rdname mfcl-methods
 #' @aliases mfcl
 
 setMethod("mfcl", signature(frq="MFCLFrq", par="MFCLPar"), 
-  function(frq, par, outpar="out.par", switch=NULL){
+  function(frq, par, outpar="out.par", switch=NULL, ...){
   
   path <- system.file("extdata",package="FLR4MFCL")
   system(paste(path, "/mfclo64 --version", sep=""))  
