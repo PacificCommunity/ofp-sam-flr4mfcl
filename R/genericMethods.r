07c1233492a74417cdbad03960161df56c1df184
robscott3 <roberts2@spc.int>
2015-10-26 10:28:46 +1100

    mfcl defined

--- R/genericMethods.r
+++ R/genericMethods.r
@@ -25,3 +25,32 @@
 #' write(MFCLFrqStats())
 
 setGeneric('write', function(x, file, append=F, ...) standardGeneric('write')) 
+
+
+
+#' mfcl
+#'
+#' Runs MFCL with defined inputs
+#'
+#' @param frq:    An object of class MFCLFrq.
+#' @param par:    An object of class MFCLPar.
+#' @param outpar: The name of the output par file
+#' @param switch: Optional numeric vector of additional flag settings
+#'
+#' @param ... Additional argument list that might not ever
+#'  be used.
+#'
+#' @return Creates a text file at the specified location.
+#' 
+#' @seealso \code{\link{read.MFCLFrq}} and \code{\link{read.MFCLPar}}
+#' 
+#' @export
+#' @docType methods
+#' @rdname mfcl-methods
+#'
+#' @examples
+#' write(MFCLFrqStats())
+
+setGeneric('mfcl', function(frq, par, outpar, switch, ...) standardGeneric('mfcl')) 
+
+
