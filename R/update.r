6eb840c53027a85f3b7fdf46df9a52679cbd0287
robscott3 <robertsc@spc.int>
2016-02-19 17:18:45 +1100

    bug fixes for window tag

--- R/update.r
+++ R/update.r
@@ -27,9 +27,7 @@
 
 
 
-setMethod("update", signature(object="MFCLFrq"), 
-          function(object, years, fisheries, multiplier, quantity, ...){
-            
+setMethod("update", signature(object="MFCLFrq"), function(object, years, fisheries, multiplier, quantity, ...){
             
   freq(object)[is.element(freq(object)$year, years) & is.element(freq(object)$fishery, fisheries) & freq(object)[,quantity]!=-1, quantity] <- 
       freq(object)[is.element(freq(object)$year, years) & is.element(freq(object)$fishery, fisheries) & freq(object)[,quantity]!=-1, quantity]*multiplier
@@ -40,5 +38,26 @@ setMethod("update", signature(object="MFCLFrq"),
 
 
 
+
+setMethod("update", signature(object="MFCLIni"), function(object, tag.obj, ...){
+  
+  if(!is(tag.obj)=="MFCLTag")
+    stop("Error: tag.obj must be a valid MFCLTag object")
+  
+  if(dim(tag_fish_rep_rate(object))[1] < release_groups(tag.obj))
+    stop("Error: Tag release groups in MFCLTag object greater than MFCLIni object")
+  
+  trg   <- release_groups(tag.obj) 
+  last <- dim(tag_fish_rep_rate(object))[1] 
+  
+  tag_fish_rep_rate(object)  <- tag_fish_rep_rate(object)[c(1:trg,last), ]
+  tag_fish_rep_grp(object)   <- tag_fish_rep_grp(object)[c(1:trg,last), ]
+  tag_fish_rep_flags(object) <- tag_fish_rep_flags(object)[c(1:trg,last), ]
+  tag_fish_rep_target(object)<- tag_fish_rep_target(object)[c(1:trg,last),]
+  tag_fish_rep_pen(object)   <- tag_fish_rep_pen(object)[c(1:trg,last), ]
+  
+  return(object)
+  
+})
 
 
