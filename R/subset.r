ba01d18536f3f5c95e35c9ab4f015fddf58e62f6
robscott3 <robert.dryden.scott@gmail.com>
2021-11-07 16:13:46 +1100

    subset for tag stuff

--- R/subset.r
+++ R/subset.r
@@ -42,3 +42,40 @@ setMethod("subset", signature(x="MFCLFrq"),
             return(res)
           })
 
+
+
+setMethod("subset", signature(x="MFCLTag"),
+         function(x, ...){
+            #browser()
+            rel <- releases(x)
+            rec <- recaptures(x)
+            
+            # apply the subset to both releases and recaptures
+            rel <- subset(rel, ...)
+            #rec <- subset(rec, ...)
+            
+            if(nrow(rel)==0)
+              stop("Error: No release groups for current selection")
+            
+            # re-organise the object so everything is compatible with the revised data
+            if(nrow(rel) != nrow(releases(x))){
+              rel <- rel[order(rel$rel.group),]
+              rel$new.rel.group <- rep(1:length(table(rel$rel.group)), table(rel$rel.group))  # re-number the release groups in ascending order
+              recnew <- merge(rec, rel[,c(1:5,8)])
+              recnew$rel.group <- recnew$new.rel.group
+            }
+            releases(x)   <- rel
+            recaptures(x) <- recnew
+            release_groups(x) <- max(releases(x)$rel.group)
+            # count the number of strata in the recaptures (i.e. the unique year, season, region, length, etc)
+            recoveries.obs      <- as.data.frame(table(recaptures(x)$rel.group))
+            recoveries.missing  <- data.frame(Var1=as.factor((1:release_groups(x))[!is.element(1:release_groups(x), recoveries.obs$Var1)]), Freq=0)
+            recoveries.all      <- rbind(recoveries.obs, recoveries.missing)
+            recoveries(x)       <- recoveries.all$Freq[order(as.numeric(as.character(recoveries.all$Var1)))]
+            
+            range(x)[c('minyear', 'maxyear')] <- c(min(releases(x)$year), max(c(releases(x)$year, recaptures(x)$recap.year)))
+            return(x)
+          })
+
+
+
