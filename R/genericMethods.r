c71a38b2731e18dcd4ead7c9172a047d7806ad7d
Arni Magnusson <thisisarni@gmail.com>
2022-08-18 16:18:58 +1100

    Don't run checkUnitDimnames() example

--- R/genericMethods.r
+++ R/genericMethods.r
@@ -299,25 +299,23 @@ setMethod('modifyRRini', signature(ini='MFCLIni',tag='MFCLTag',rr='data.frame',P
 
 #' checkUnitDimnames
 #'
-#' Re-orders the unit dimnames in ascending order instead 
+#' Re-orders the unit dimnames in ascending order instead
 #' of default ordering of FLQuant method which uses the year dimension
 #'
 #' @param obj An object of class MFCL eg. MFCLFrq, MFCLPar, etc.
 #' @param nfisheries The number of fisheries in the model
 #'
-#' @param ... Additional argument list that might not ever
-#'  be used.
+#' @param ... Additional argument list that might not ever be used.
 #'
 #' @return Returns an object of the same class but with FLQuant unit dimension
 #' re-ordered as appropriate.
-#' 
-#' 
 #'
 #' @examples
+#' \dontrun{
 #' checkUnitDimnames(MFCLRep())
+#' }
 
-
-checkUnitDimnames <-    function(obj,nfisheries){
+checkUnitDimnames <- function(obj,nfisheries){
       unit_order <- as.character(1:nfisheries)
       # ID FLQs with unit dimensions
       repslots <- getSlots(class(obj))
