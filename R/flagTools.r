86e35df3a85dbefd93b77c028e22a5b77e71e260
Arni Magnusson <thisisarni@gmail.com>
2022-09-23 12:16:15 +1100

    Minor improvements to style, no changes in functionality

--- R/flagTools.r
+++ R/flagTools.r
@@ -1,13 +1,13 @@
-#FLR4MFCL - R4MFCL built with FLR classes
-#Copyright (C) 2018  Rob Scott
+# FLR4MFCL - R4MFCL built with FLR classes
+# Copyright (C) 2018  Rob Scott
 
-#' recruitment periods
+#' Recruitment Periods
 #'
-#' Calculates recruitment periods for deterministic and stochastic projection settings
+#' Calculate recruitment periods for deterministic and stochastic projection settings.
 #'
-#' @param parfile An object of class MFCLPar
+#' @param par An object of class MFCLPar.
 #'
-#' @return An object of class numeric vector
+#' @return An object of class numeric vector.
 #'
 #' @examples
 #' data(par)
@@ -15,32 +15,32 @@
 #'
 #' @export
 
+recPeriod <- function(par, af199=NULL, af200=NULL, pf232=NULL, pf233=NULL, show=FALSE){
 
-recPeriod <- function(par, af199=NULL, af200=NULL, pf232=NULL, pf233=NULL, show=F){
- 
-  mat_d <- matrix(rev(1:dimensions(par)["years"]), ncol=dimensions(par)["seasons"], byrow=T)
+  mat_d <- matrix(rev(1:dimensions(par)["years"]), ncol=dimensions(par)["seasons"], byrow=TRUE)
   rownames(mat_d) <- as.character(range(par)["minyear"]:range(par)["maxyear"])
-  
-  mat_s <- matrix(1:dimensions(par)["years"], ncol=dimensions(par)["seasons"], byrow=T)
+
+  mat_s <- matrix(1:dimensions(par)["years"], ncol=dimensions(par)["seasons"], byrow=TRUE)
   rownames(mat_s) <- as.character(range(par)["minyear"]:range(par)["maxyear"])
-  
-  if(show==T){
+
+  if(show){
     print("Deterministic"); print(mat_d)
     print("Stochastic")   ; print(mat_s)
     print("Flags")
   }
-  # if flag values given as args : calculate corresponding values
-  if(!is.null(af199) & is.null(pf232))
+
+  # If flag values given as args : calculate corresponding values
+  if(!is.null(af199) && is.null(pf232))
     pf232 <- mat_s[mat_d==af199]
-  if(!is.null(af200) & is.null(pf233))
+  if(!is.null(af200) && is.null(pf233))
     pf233 <- mat_s[mat_d==af200]
-  
-  if(!is.null(pf232) & is.null(af199))
+
+  if(!is.null(pf232) && is.null(af199))
     af199 <- mat_s[mat_d==pf232]
-  if(!is.null(pf233) & is.null(af200))
+  if(!is.null(pf233) && is.null(af200))
     af200 <- mat_s[mat_d==pf233]
-  
-  # if no flagvals given as args : use values given in the par
+
+  # If no flagvals given as args : use values given in the par
   if(is.null(af199))
     af199 <- flagval(par, 2, 199)$value
   if(is.null(af200))
@@ -49,24 +49,23 @@ recPeriod <- function(par, af199=NULL, af200=NULL, pf232=NULL, pf233=NULL, show=
     pf232 <- flagval(par, 1, 232)$value
   if(is.null(pf233))
     pf233 <- flagval(par, 1, 233)$value
-  
+
   res <- c(af199, af200, pf232, pf233)
   names(res) <- c('af199', 'af200', 'pf232', 'pf233')
-  
+
   return(res)
-  
 }
-  
 
 
-#' flag summary
+
+#' Flag Summary
 #'
-#' flag settings summarised by MFCL User Guide sections
+#' Flag settings summarised by MFCL User Guide sections.
 #'
-#' @param par An object of class MFCLPar
-#' @param type A character string specifying the MFCL User Guide section
+#' @param par An object of class MFCLPar.
+#' @param type A character string specifying the MFCL User Guide section.
 #'
-#' @return A data frame of flag settings
+#' @return A data frame of flag settings.
 #'
 #' @examples
 #' data(par)
@@ -75,30 +74,30 @@ recPeriod <- function(par, af199=NULL, af200=NULL, pf232=NULL, pf233=NULL, show=
 #' @export
 
 flagSummary <- function(par, type){
-  
+
   options <- c('projection', 'impact_analysis', 'MSY')
-  
+
   if(!is.element(type, options))
     stop(paste("arg 'type' must be one of the following:", paste0(options, collapse="; ")))
-  
-  ffrange <- -1:-dimensions(par)['fisheries']
+
+  ffrange <- -(1:dimensions(par)['fisheries'])
   switch(type,
     "projection"     = rbind(flagval(par, 1, c(142, 231:239)), flagval(par, 2, c(20, 190, 191, 195, 161, 199, 200))),
     "impact_analysis"= rbind(flagval(par, 2, c(170:176, 190, 191, 193)), flagval(par, ffrange, 55)),
     "MSY"            = rbind(flagval(par, 2, c(112, 140:141, 145:155, 161:163, 165:169, 194, 199:200)), flagval(par, ffrange, 70))
-  )  
+  )
 }
 
 
 
-#' flagDiff
+#' Flag Diff
 #'
-#' flag differences between two par files
+#' Show flag differences between two par objects/files.
 #'
-#' @param par An object of class MFCLPar
-#' @param par An object of class MFCLPar
+#' @param par1 An object of class MFCLPar.
+#' @param par2 An object of class MFCLPar.
 #'
-#' @return A data frame of flag settings for par1 and par2
+#' @return A data frame of flag settings for par1 and par2.
 #'
 #' @examples
 #' data(par)
@@ -109,10 +108,10 @@ flagSummary <- function(par, type){
 #' @export
 
 flagDiff <- function(par1, par2){
-  
-  res <- flags(par1)[flags(par1)$value!=flags(par2)$value,]
-  res <- cbind(res, flags(par2)[flags(par1)$value!=flags(par2)$value, 'value'])
-  colnames(res) <- c("flagtype","flag","par1","par2")
-  
+
+  res <- flags(par1)[flags(par1)$value != flags(par2)$value,]
+  res <- cbind(res, flags(par2)[flags(par1)$value != flags(par2)$value, 'value'])
+  colnames(res) <- c("flagtype", "flag", "par1", "par2")
+
   return(res)
 }
