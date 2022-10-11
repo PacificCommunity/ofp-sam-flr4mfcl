d6e0a74a37ca970e96c6ea70eee3a987baa07646
Arni Magnusson <thisisarni@gmail.com>
2022-10-11 11:22:22 +1100

    Pass argument hcr_func

--- R/hcrs.R
+++ R/hcrs.R
@@ -228,7 +228,7 @@ hcr_threshold_constrained <- function(sbsbf0, params, reference_out){
 
 hcr_hillary_step_constrained <- function(sbsbf0, params, reference_out){
   #browser()
-  out <- hcr_constrained(sbsbf0 = sbsbf0, params=params, reference_out=reference_out, hcr_fun="hcr_hillary_step")
+  out <- hcr_constrained(sbsbf0 = sbsbf0, params=params, reference_out=reference_out, hcr_func="hcr_hillary_step")
 }
 
 
@@ -283,7 +283,7 @@ hcr_asymptotic_hillary_step <- function(sbsbf0, params){
 #' @rdname hcr_funcs
 
 hcr_asymptotic_hillary_step_constrained <- function(sbsbf0, params, reference_out){
-  out <- hcr_constrained(sbsbf0 = sbsbf0, params=params, reference_out=reference_out, hcr_fun="hcr_asymptotic_hillary_step")
+  out <- hcr_constrained(sbsbf0 = sbsbf0, params=params, reference_out=reference_out, hcr_func="hcr_asymptotic_hillary_step")
 }
 
 
