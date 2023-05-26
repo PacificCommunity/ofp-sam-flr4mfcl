779d4d616bf6439724566a8f3eb0159ce76ef857
robscott3 <robersc@spc.int>
2023-05-27 10:09:39 +1100

    adding read.MFCLALK and read.MFCLWgtFit

---
+++ R/read.MFCLALK.r
@@ -0,0 +1,72 @@
+#FLR4MFCL - R4MFCL built with FLR classes
+#Copyright (C) 2018  Rob Scott
+
+#' read.MFCLALK
+#'
+#' Reads information from the conditional age length input file and creates an MFCLALK object.
+#'
+#' @param inifile A character string giving the name and path of the conditional age length input file to be read.
+#'
+#' @return An object of class MFCLALK
+#'
+#' @examples
+#' \dontrun{
+#' read.MFCLALK("C:/R4MFCL/test_data/skj_ref_case/yft.age_length")
+#' }
+#'
+#' @export
+
+# alfile <- "/media/sf_assessments/yft/2023/model_runs/stepwise/03_PreCatchCond/03j_No_Effort_Projections/yft.age_length"
+# lenfit <- "/media/sf_assessments/yft/2023/model_runs/stepwise/03_PreCatchCond/03j_No_Effort_Projections/length.fit"
+
+read.MFCLALK <- function(alfile, lenfit=NULL) {
+  
+  trim.leading  <- function(x) sub("^\\s+", "", x) 
+  splitter      <- function(ff, tt, ll=1, inst=1) unlist(strsplit(trim.leading(ff[grep(tt, ff)[inst]+ll]),split="[[:blank:]]+")) 
+  
+  if(is.character(lenfit))
+    lenfit <- read.MFCLLenFit2(lenfit)
+  
+  alk <- MFCLALK()
+  
+  alf <- readLines(alfile)   # read file
+  alf <- alf[nchar(alf)>=1]  # and remove all blank lines
+  
+  nrecords <- as.numeric(alf[grep("# num age length records", alf)+1])
+  
+  markers <- grep("# Year", alf)
+  if(length(markers) != nrecords)
+    warning(paste("Number of ALKs (",length(markers),") not consistent with 'num age length records' value (", nrecords, ")"))
+  
+  nlengths <- markers[2]-markers[1] -2 
+  nages    <- length(unlist(strsplit(alf[markers[1]+2], split="[[:blank:]]+")))
+  
+  lbins    <- NA
+  if(!is.null(lenfit))
+    lbins <- sort(unique(lenfits(lenfit)$length))
+  
+  if(is.null(lenfit))
+    warning("lbins not specified")
+  
+  if(!is.null(lenfit) & length(lbins) != nlengths)
+    warning("lbins not consistent with alk dimensions")
+  
+  # array of ALK header information - year, month, fishery, ...
+  hdat   <- t(array(as.numeric(unlist(strsplit(paste(alf[markers+1], collapse= " "), split="[[:blank:]]+"))), dim=c(4, nrecords)))
+  # vector of observations - lots of zeroes!
+  alkdat <- unlist(lapply(markers+2, function(mm){as.numeric(unlist(strsplit(alf[mm:(mm+nlengths-1)], split="[[:blank:]]+")))}))
+  
+  slot(alk, 'ESS') <- as.numeric(splitter(alf, "# effective sample size"))
+  slot(alk, 'ALK') <- data.frame(year   =rep(hdat[,1], each=nages*nlengths),
+                                 month  =rep(hdat[,2], each=nages*nlengths),
+                                 fishery=rep(hdat[,3], each=nages*nlengths),
+                                 species=rep(hdat[,4], each=nages*nlengths),
+                                 age    =1:nages,
+                                 length =rep(lbins, each=nages),
+                                 obs    =alkdat)
+
+  slot(alk, 'range') <- unlist(list(minage=1,maxage=nages,plusgroup=NA,minlength=min(lbins),maxlength=max(lbins),minyear=min(hdat[,1]),maxyear=max(hdat[,1])))
+  
+  return(alk)
+}
+    
---
+++ R/read.MFCLWgtFit.r
@@ -0,0 +1,149 @@
+#FLR4MFCL - R4MFCL built with FLR classes
+#Copyright (C) 2018  Rob Scott
+
+#' read.MFCLWgtFit
+#'
+#' Reads information from the weight.fit file and creates an MFCLWgtFit object.
+#'
+#' @param inifile A character string giving the name and path of the weight.fit file to be read.
+#'
+#' @return An object of class MFCLWgtFit
+#'
+#' @examples
+#' \dontrun{
+#' read.MFCLWgtFit("C:/R4MFCL/test_data/skj_ref_case/weight.fit")
+#' }
+#'
+#' @export
+
+# wffile <- "/media/sf_assessments/yft/2023/model_runs/stepwise/03_PreCatchCond/03j_No_Effort_Projections/weight.fit"
+
+read.MFCLWgtFit <- function(wffile, get_wtage=FALSE) {
+  
+  return(read.MFCLWgtFit2(wffile, get_wtage))
+}  
+  
+
+
+#  kk <- read.MFCLWgtFit2("/media/sf_assessments/bet/2023/model_runs/stepwise/02PreCatchCond/B06CatchCond4/weight.fit")
+  
+# Speedier version of the original read.MFCLLenFit2
+# With option not to get the predicted age-length data.frame (as it is huge)
+read.MFCLWgtFit2 <- function(wffile, get_wgtage = FALSE) {
+
+  # Helper function to clean up character strings
+  trim.leading  <- function(x) sub("^\\s+", "", x) 
+  
+  wff <- readLines(wffile)   # read file
+  wff <- wff[nchar(wff)>=1]  # and remove all blank lines
+  
+  # First line is # FIT X, the version number
+  vno <- wff[1] # This code may only work for FIT 3 versions
+  if (vno != "# FIT 3"){
+    warning("This method was expecting version '# FIT 3' in the first line\n. It may not work properly with whatever version your file is.")
+  }
+  
+  # remove comments to make similar format to length.fit
+  wff <- wff[-grep("#wghtsum", wff)]
+  wff <- wff[-grep("#wghtfrq", wff)]
+  
+  # Next line is data for drawing histograms in the viewer and can be ignored (see MFCL manual)
+  # Then there are the length bins
+  weight_intervals <-  as.numeric(unlist(strsplit(trim.leading(wff[3]), split="[[:blank:]]+")))
+  names(weight_intervals) <- c("no_intervals", "smallest_weight", "interval")
+  wbins <- seq(from=weight_intervals[2], length=weight_intervals[1], by=weight_intervals[3])
+  # Then integer of number of fisheries + 1 - ignore
+  # Then samples per fishery - vector of length nfisheries + 1
+  # Read and drop the last one
+  fsh_nobs <- rev(rev(as.numeric(unlist(strsplit(trim.leading(wff[5]), split="[[:blank:]]+"))))[-1])
+  # No. ages
+  ages    <- 1:as.numeric(wff[6])
+  nages <- length(ages)
+  # Ignore fishery species pointer (lines 7 & 8)
+
+  # Next: the fishery blocks - (nfisheries + 1) blocks (fishery 1, fishery 2, ..., fishery totals)
+  fishery_blocks <- grep("fishery", wff)
+  # Each fishery block is made up of sample blocks (see fsh_nobs for n samples per fishery)
+  # Each sample block = 7 lines + matrix with nages rows
+  sample_size <- nages + 9 
+
+  # Go fishery by fishery, and process all sample blocks at the same time
+  nfisheries <- length(fsh_nobs)
+  out <- list()
+  out2 <- list()
+
+  for (fishery in 1:nfisheries){
+    if(fsh_nobs[fishery] == 0)
+      next
+      
+    fishery_start <- fishery_blocks[fishery]
+    # Get year and month of each sample block in each fishery
+    time_ids_posns <- fishery_start + seq(from = 1, to = sample_size * fsh_nobs[fishery], by=sample_size)
+    time_ids <- wff[time_ids_posns]
+    time_ids <- strsplit(trim.leading(time_ids), split="[[:blank:]]+")
+    year <- as.numeric(unlist(lapply(time_ids, "[[", 1)))
+    month <- as.numeric(unlist(lapply(time_ids, "[[", 2)))
+    # Get nsamples
+    nsample_posns <- fishery_start + seq(from = 3, to = sample_size * fsh_nobs[fishery], by=sample_size)
+    nsamples <- wff[nsample_posns]
+    nsamples <- as.numeric(unlist(strsplit(trim.leading(nsamples), split="[[:blank:]]+")))
+  
+    # prop_obs - 1 line per sample block
+    obs_posns <- fishery_start +  seq(from = 6, to = (sample_size* (fsh_nobs[fishery] - 1)) + 6, by=sample_size)
+    prop_obs <- wff[obs_posns]
+    prop_obs <- as.numeric(unlist(strsplit(trim.leading(prop_obs), split="[[:blank:]]+")))
+    # prop_pred - 1 line per sample block
+    pred_posns <- fishery_start +  seq(from = 8, to = (sample_size*(fsh_nobs[fishery] - 1)) + 8, by=sample_size)
+    prop_pred <- wff[pred_posns]
+    prop_pred <- as.numeric(unlist(strsplit(trim.leading(prop_pred), split="[[:blank:]]+")))
+  
+    # Put together into data.frame
+    temp_df <- data.frame(fishery = fishery,
+                            year = rep(year, each=length(wbins)),
+                             month = rep(month, each=length(wbins)),
+                             sample_size = rep(nsamples, each=length(wbins)),
+                             weight = rep(wbins, fsh_nobs[fishery]),
+                             obs = prop_obs,
+                             pred = prop_pred)
+    out[[fishery]] <- temp_df
+  
+    if(get_wgtage == TRUE){
+      # The huge matrix of predicted props by age and length
+      # Each matrix has nages rows, and length(lbins) columns
+      # Each sample block has a matrix
+      matrix_starts <- fishery_start + seq(from = 10, to=sample_size * fsh_nobs[fishery], by=sample_size)
+      # From each start read in nages lines
+      matrix_posns <- unlist(lapply(matrix_starts, function(x) return(x:(x+nages-1))))
+      matdat <- wff[matrix_posns]
+      matdat <- as.numeric(unlist(strsplit(trim.leading(matdat), split="[[:blank:]]+")))
+    
+      tempmatdf <- data.frame(fishery = fishery,
+        year =rep(year, each=length(wbins)*nages),
+        month =rep(month, each=length(wbins)*nages),
+        weight = rep(rep(wbins, by=nages), fsh_nobs[fishery]),
+        age  = rep(rep(ages, each=length(wbins)), fsh_nobs[fishery]),
+        pred = matdat)
+      
+      out2[[fishery]] <- tempmatdf
+    }
+  }
+  
+  wgtdat <- do.call("rbind", out)
+  agewgtdat <- do.call("rbind", out2)
+  
+  # Get range
+  range <- c(ages[1], ages[length(ages)], NA, min(wgtdat$year), max(wgtdat$year))
+  # Get length-at-age - in the fishery blocks but same for each fishery
+  
+  waa <- wff[grep(paste0("# fishery ", which(fsh_nobs>0)[1]), wff)[1] + 4]
+  waa <- as.numeric(unlist(strsplit(trim.leading(waa), split="[[:blank:]]+")))
+  waa <- FLQuant(waa, dimnames=list(age=as.character(ages), year="all", unit="unique", season="all", area="unique"))
+  
+  # Make new object and populate
+  obj <- MFCLWgtFit()
+  slot(obj, 'range')[] <- range
+  slot(obj, 'waa') <- waa
+  slot(obj, 'wgtfits') <- wgtdat
+  slot(obj, 'wgtagefits') <- as.data.frame(agewgtdat) # Force as data frame in case it is NULL (get_lenage==FALSE)
+  return(obj)
+}
