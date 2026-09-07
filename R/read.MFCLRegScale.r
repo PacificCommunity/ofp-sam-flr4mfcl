0af593a3d3054477dad49fd4631bc610accb351c
Arni Magnusson <thisisarni@gmail.com>
2026-09-07 20:22:30 +1100

    Remove whitespace

--- R/read.MFCLRegScale.r
+++ R/read.MFCLRegScale.r
@@ -21,30 +21,29 @@
 # yftregscale <- read.MFCLRegScale(file)
 
 read.MFCLRegScale <- function(file, seasons=4, ...) {
-  
-  trim.leading  <- function(x) sub("^\\s+", "", x) 
-  splitter      <- function(ff, tt, ll=1, inst=1) unlist(strsplit(trim.leading(ff[grep(tt, ff)[inst]+ll]),split="[[:blank:]]+")) 
-  
+
+  trim.leading  <- function(x) sub("^\\s+", "", x)
+  splitter      <- function(ff, tt, ll=1, inst=1) unlist(strsplit(trim.leading(ff[grep(tt, ff)[inst]+ll]),split="[[:blank:]]+"))
+
   regscale <- MFCLRegScale()
-  
+
   rgf <- readLines(file)   # read file
   rgf <- rgf[nchar(rgf)>=1]  # and remove all blank lines
   if(any(grepl("# ", rgf) & nchar(rgf)<3))
     rgf <- rgf[-seq(1,length(rgf))[grepl("# ", rgf) & nchar(rgf)<3]]   # remove single hashes with no text "# "
-  
+
   range(regscale)[c('minyear', 'minmonth', 'maxyear', 'maxmonth')] <- as.numeric(unlist(strsplit(trim.leading(rgf[[1]]), split="[[:blank:]]+")))
-  
+
   dimensions(regscale)['seasons'] <- seasons
   dimensions(regscale)['years']   <- (range(regscale)['maxyear'] - range(regscale)['minyear'] + 1) * seasons
   dimensions(regscale)['regions'] <- length(unlist(strsplit(rgf[2], split="[[:blank:]]+")))
-  
+
   # checks
   if(dimensions(regscale)['years'] != length(rgf)-1)
     warning('You might want to check your inputs - year ranges might not be lining up')
-  
+
   index(regscale) <- t(array(round(as.double(unlist(strsplit(rgf[2:length(rgf)], split="[[:blank:]]+"))),3),
                              dim=c(dimensions(regscale)['regions'], length(rgf)-1)))
 
   return(regscale)
 }
-    
